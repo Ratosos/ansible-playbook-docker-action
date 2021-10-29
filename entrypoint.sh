43ecdf8e4ec51faf287c625bea949484645d97f2
Ratosos <80895893+Ratosos@users.noreply.github.com>
2021-10-29 17:29:22 +0100

    Trocar para azure.azcollection modules

--- entrypoint.sh
+++ entrypoint.sh
@@ -81,7 +81,7 @@ else
     echo "\$INPUT_GALAXYGITHUBTOKEN not set. Won't do any authentication for roles installation."
   fi
 
-  ansible-galaxy collection install community.azure --force \
+  ansible-galaxy collection install azure.azcollection --force \
 #    -p ${ROLES_PATH} \
 #    -r ${REQUIREMENTS} \
 #    ${VERBOSITY}
